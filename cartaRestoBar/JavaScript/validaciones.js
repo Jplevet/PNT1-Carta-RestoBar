a5de12af04a19e1f7d2247be8c748fe8a7ddf6ea
Javier Santagiuliana <77638530+Santagiulianaj@users.noreply.github.com>
2022-09-21 11:27:39 -0300

    Fix mail validation

--- cartaRestoBar/JavaScript/validaciones.js
+++ cartaRestoBar/JavaScript/validaciones.js
@@ -1,23 +1,17 @@
 function validarFormulario(){
  
   var nom = document.getElementById('nombre').value;
-  console.log("linea 4");
   if(nom.length == 0) {
     alert('Nombre vacío');
     return;
   }
-  console.log("linea 4");
-  
-    let correo = document.forms["formulario"]["correo"];
-    var mailformat = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+  let correo = document.forms["formulario"]["correo"];
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (correo.value.match(mailformat)) {
-        return true;
-    } else {
-    alert("Email incorrecto!");
-        return false;
-    }
-    
-  
-    
+  if (correo.value.match(mailformat)) {
+      return true;
+  } else {
+  alert("Email incorrecto!");
+      return false;
+  }
 }
